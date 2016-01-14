4290aa205196afc784e3b9eccedda989ee653f3b
konstantin <krivlenia@targetprocess.com>
2016-01-14 20:26:33 +0300

    version 1.0.0

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-//var replace = require('./src/replace');
 module.exports = {
     book: {
         assets: "./src",
         js: [
             "plugin.js"
         ]
-    }/*,
-    hooks: {
-        'page': function (page) {
-            var config = this.getConfig('pluginsConfig') || {};
-            page.sections[0].content = replace(page.sections[0].content, config.jsfiddle);
-            return page;
-        }
-    }*/
+    }
 };
